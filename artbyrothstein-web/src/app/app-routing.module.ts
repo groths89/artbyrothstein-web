52cdc4ceb505fe3b9c8c0c2349d64c8ca0b32ea0
Gregory Rothstein <gregory@thecommodoredesigns.com>
2020-05-06 21:19:14 -0400

    added routing to website with all pages

--- artbyrothstein-web/src/app/app-routing.module.ts
+++ artbyrothstein-web/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ExhibitsComponent } from './exhibits/exhibits.component';
+import { AboutComponent } from './about/about.component';
+import { ShopComponent } from './shop/shop.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',   redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'exhibits', component: ExhibitsComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'shop', component: ShopComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- artbyrothstein-web/src/app/app.module.ts
+++ artbyrothstein-web/src/app/app.module.ts
@@ -9,6 +9,13 @@ import { AppFooterComponent } from './app-footer/app-footer.component';
 import { AppWelcomeComponent } from './app-welcome/app-welcome.component';
 import { AppNewestExhibitComponent } from './app-newest-exhibit/app-newest-exhibit.component';
 import { AppPortfolioSamplesComponent } from './app-portfolio-samples/app-portfolio-samples.component';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ExhibitsComponent } from './exhibits/exhibits.component';
+import { AboutComponent } from './about/about.component';
+import { ShopComponent } from './shop/shop.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +25,14 @@ import { AppPortfolioSamplesComponent } from './app-portfolio-samples/app-portfo
     AppFooterComponent,
     AppWelcomeComponent,
     AppNewestExhibitComponent,
-    AppPortfolioSamplesComponent
+    AppPortfolioSamplesComponent,
+    HomeComponent,
+    GalleryComponent,
+    ExhibitsComponent,
+    AboutComponent,
+    ShopComponent,
+    ContactComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
